76d808e3a29d02eac3aadc1b4175fd258fa134f8
zb1006801184 <1006801184@qq.com>
2017-06-16 15:57:04 +0800

    Initial Commit

---
+++ 旧卖货神器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  旧卖货神器
+//
+//  Created by CYT on 2017/6/16.
+//  Copyright © 2017年 CYT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 旧卖货神器/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  旧卖货神器
+//
+//  Created by CYT on 2017/6/16.
+//  Copyright © 2017年 CYT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
